7c88c9ee9e31fb971222cfd3cb64599b1aa89ef8
Yuliya S <safronova.yuv@gmail.com>
2024-02-05 15:54:36 +0400

    feat: create movie schema

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,67 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const movieSchema = new mongoose.Schema();
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL картинки',
+    },
+    required: true,
+  },
+  trailerLink: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL трейлера',
+    },
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL миниатюры',
+    },
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('user', movieSchema);
